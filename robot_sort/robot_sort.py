70f6dc0caa5d3aef77ee4849401460fd83463fcb
Carlos Santana <carlossantana987@outlook.com>
2019-10-11 12:18:45 -0400

    I may have forced the robot.IDk it cant be this easy. is it?

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,8 +97,13 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        arr = self._list
 
+        for i in range(0,len(arr)-1):
+            for j in range(0, len(arr)-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1]=arr[j+1], arr[j]
+        return arr
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
